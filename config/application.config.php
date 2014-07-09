704a7b5e53b6ac6e968212b136a2c2c1277ba844
eddylecca <eddy.lecca3@gmail.com>
2014-07-09 08:48:49 -0500

    Añadiendo Módulo Noticias

--- config/application.config.php
+++ config/application.config.php
@@ -6,6 +6,7 @@ return array(
         'Application',
         'Admin',
         'Page',
+        'Noticias',
     ),
 
     // These are various options for the listeners attached to the ModuleManager
