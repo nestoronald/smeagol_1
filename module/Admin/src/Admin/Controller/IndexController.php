36cae12f62ab2160ce434216294200563dada128
eddylecca <eddy.lecca3@gmail.com>
2014-07-09 09:09:56 -0500

    7.3.- Validación de Acceso al módulo Admin

--- module/Admin/src/Admin/Controller/IndexController.php
+++ module/Admin/src/Admin/Controller/IndexController.php
@@ -3,11 +3,22 @@
 namespace Admin\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
+use Zend\Authentication\AuthenticationService;
 use Zend\View\Model\ViewModel;
 
 class IndexController extends AbstractActionController
 {
     public function indexAction()
     {
+
+    	$auth = new AuthenticationService();
+    	if (!$auth->hasIdentity()) {
+    		$this->redirect()->toRoute('auth');
+    	}   
+    	// Identity exists; get it
+    	$identity = $auth->getIdentity();
+    	
+    	//pasando variable a la vista
+    	return array("user"=>$identity);
     }
 }
