f8d49d3a3b5cba31d81fe7f41bb12287fbceb16f
eddylecca <eddy.lecca3@gmail.com>
2014-07-11 21:17:19 -0500

    8.4.- Despliegue del Menú en el Layout

--- sql/smeagol_create.sql
+++ sql/smeagol_create.sql
@@ -93,3 +93,35 @@ DEFAULT CHARACTER SET = utf8;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+-- -----------------------------------------------------
+-- Table `smeagol`.`menu`
+-- -----------------------------------------------------
+CREATE  TABLE IF NOT EXISTS `smeagol`.`menu` (
+
+  `id` INT NOT NULL AUTO_INCREMENT ,
+
+  `name` VARCHAR(100) NULL ,
+
+  `parent_id` INT NOT NULL DEFAULT 0 ,
+
+  `order_id` INT NOT NULL DEFAULT 0 ,
+
+  `node_id` INT NOT NULL ,
+
+  PRIMARY KEY (`id`) ,
+
+  INDEX `fk_menu_node1_idx` (`node_id` ASC) ,
+
+  CONSTRAINT `fk_menu_node1`
+
+    FOREIGN KEY (`node_id` )
+
+    REFERENCES `smeagol`.`node` (`id` )
+
+    ON DELETE NO ACTION
+
+    ON UPDATE NO ACTION)
+
+ENGINE = InnoDB;
